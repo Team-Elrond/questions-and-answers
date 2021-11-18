9fb3c601e5e2e73f55e62209b684467d368e6ec1
Joshua Booth <joshua.n.booth@gmail.com>
2021-11-18 10:00:25 -0800

    Implement GET /qa/questions

--- routes/get.js
+++ routes/get.js
@@ -33,4 +33,59 @@ router.get('/qa/questions/:question_id/answers', asyncTry(async (req, res) => {
   });
 }));
 
+const stmtGetQuestions = `
+  SELECT question_id, question_body, question_date, asker_name, question_helpfulness
+    FROM question
+    WHERE product_id=$1::INT AND reported=FALSE
+    ORDER BY question_id ASC
+    LIMIT $2::INT
+    OFFSET $3::INT
+`;
+const stmtGetAnswersToQuestions = `
+  SELECT id, body, date, answerer_name, helpfulness, photos, answer.question_id
+    FROM answer
+    INNER JOIN question
+    ON answer.question_id = question.question_id
+    WHERE product_id=$1::INT
+      AND answer.question_id >= $2::INT
+      AND answer.question_id <= $3::INT
+    order by answer.question_id ASC
+`;
+router.get('/qa/questions', asyncTry(async (req, res) => {
+  const product_id = req.queryInt('product_id');
+  const page = req.queryInt('page', 1);
+  const count = req.queryInt('count', 5);
+  const { rows: results } = await sql.query({
+    name: 'get-questions',
+    text: stmtGetQuestions,
+    values: [product_id, count, (page - 1) * count],
+  });
+  if (results.length > 0) {
+    const questions = new Map();
+    for (const question of results) {
+      question.answers = {};
+      questions.set(question.question_id, question);
+    }
+    const minQ = results[0].question_id;
+    const maxQ = results[results.length - 1].question_id;
+    const { rows: answers } = await sql.query({
+      name: 'get-answers-to-questions',
+      text: stmtGetAnswersToQuestions,
+      values: [product_id, minQ, maxQ],
+    });
+    let question = { };
+    for (const answer of answers) {
+      if (question.question_id !== answer.question_id) {
+        question = questions.get(answer.question_id);
+      }
+      answer.photos = answer.photos.split(' ');
+      question.answers[answer.id] = answer;
+    }
+  }
+  res.json({
+    product_id: product_id.toString(),
+    results,
+  });
+}));
+
 module.exports = router;
--- routes/get.spec.js
+++ routes/get.spec.js
@@ -37,18 +37,22 @@ describe('GET /qa/questions/:question_id/answers', () => {
   it('sends results', async () => {
     await sendSample();
     pruneSample();
-    await request(app)
+
+    const res = await request(app)
       .get('/qa/questions/2/answers')
       .expect('Content-Type', /application\/json/)
-      .expect(200)
-      .expect(res => expect(res.body.results).toEqual([sample]));
+      .expect(200);
+
+    expect(res.body.results).toEqual([sample]);
   });
 
   it('sends an empty array with no results', async () => {
     await sendSample();
-    await request(app)
-      .get('/qa/questions/0/answers')
-      .expect(res => expect(res.body.results).toEqual([]));
+
+    const res = await request(app)
+      .get('/qa/questions/0/answers');
+
+    expect(res.body.results).toEqual([]);
   });
 
   it('hides reported answers', async () => {
@@ -58,9 +62,11 @@ describe('GET /qa/questions/:question_id/answers', () => {
     sample.reported = false;
     await sendSample();
     pruneSample();
-    await request(app)
-      .get('/qa/questions/2/answers')
-      .expect(res => expect(res.body.results).toEqual([sample]));
+
+    const res = await request(app)
+      .get('/qa/questions/2/answers');
+
+    expect(res.body.results).toEqual([sample]);
   });
 
   it('paginates', async () => {
@@ -69,9 +75,119 @@ describe('GET /qa/questions/:question_id/answers', () => {
       sample.body = i.toString();
       await sendSample();
     }
-    await request(app)
-      .get('/qa/questions/2/answers?count=3&page=4')
-      .expect(res => expect(res.body.results.map(row => row.body))
-        .toEqual(['10', '11', '12']));
+
+    const res = await request(app)
+      .get('/qa/questions/2/answers?count=3&page=4');
+
+    expect(res.body.results.map(row => row.body)).toEqual(['10', '11', '12']);
+  });
+});
+
+describe('GET /qa/questions', () => {
+  let sample;
+  const sendSample = () => sql.query(
+    'INSERT INTO question VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+    Object.values(sample)
+  );
+  const pruneSample = () => {
+    delete sample.reported;
+    delete sample.asker_email;
+    delete sample.product_id;
+    sample.answers = {};
+  };
+
+  beforeEach(() => {
+    sample = {
+      question_id: 1,
+      product_id: 2,
+      question_body: 'body',
+      question_date: new Date().toISOString(),
+      asker_name: 'name',
+      asker_email: 'email',
+      question_helpfulness: 3,
+      reported: false,
+    };
+  });
+
+  it('sends results', async () => {
+    await sendSample();
+    pruneSample();
+
+    const res = await request(app)
+      .get('/qa/questions?product_id=2')
+      .expect('Content-Type', /application\/json/)
+      .expect(200);
+
+    expect(res.body.results).toEqual([sample]);
+  });
+
+  it('sends an empty array with no results', async () => {
+    await sendSample();
+
+    const res = await request(app)
+      .get('/qa/questions?product_id=0');
+
+    expect(res.body.results).toEqual([]);
+  });
+
+  it('hides reported questions', async () => {
+    sample.reported = true;
+    await sendSample();
+    sample.question_id = 2;
+    sample.reported = false;
+    await sendSample();
+    pruneSample();
+
+    const res = await request(app)
+      .get('/qa/questions?product_id=2');
+
+    expect(res.body.results).toEqual([sample]);
+  });
+
+  it('paginates', async () => {
+    for (let i = 1; i <= 20; i += 1) {
+      sample.question_id = i;
+      sample.question_body = i.toString();
+      await sendSample();
+    }
+
+    const res = await request(app)
+      .get('/qa/questions?product_id=2&count=3&page=4');
+
+    expect(res.body.results.map(row => row.question_body))
+      .toEqual(['10', '11', '12']);
+  });
+
+  it('displays only the corresponding answers', async () => {
+    const answer = {
+      answer_id: 1,
+      question_id: 2,
+      body: 'body',
+      date: new Date().toISOString(),
+      answerer_name: 'name',
+      answerer_email: 'email',
+      helpfulness: 3,
+      reported: false,
+      photos: 'a b c d',
+    };
+    for (let i = 0; i <= 5; i += 1) {
+      sample.product_id = i === 0 || i === 5 ? i : 3;
+      sample.question_id = i;
+      await sendSample();
+      answer.question_id = i;
+      for (let j = 1; j <= 2; j += 1) {
+        answer.answer_id = i * 10 + j;
+        sql.query(
+          'INSERT INTO answer VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+          Object.values(answer)
+        );
+      }
+    }
+
+    const res = await request(app)
+      .get('/qa/questions?product_id=3&count=2&page=2');
+
+    expect(res.body.results.flatMap(row => Object.keys(row.answers)))
+      .toEqual(['31', '32', '41', '42']);
   });
 });
