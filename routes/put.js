d6d5fae7f0f7109551762c38ca2cdfc0f3c9b647
Joshua Booth <joshua.n.booth@gmail.com>
2021-11-18 16:20:05 -0800

    Cleanup

--- routes/put.js
+++ routes/put.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const sql = require('../sql');
-const { asyncTry } = require('../middleware');
 
 const router = express.Router();
+
+async function tryPut(res, query) {
+  try {
+    const { rowCount } = await sql.query(query);
+    res.sendStatus(rowCount === 0 ? 404 : 204);
+  } catch (err) {
+    console.error(err.stack);
+    res.status(500).send(err.message);
+  }
+}
 
 const stmtReportQuestion = `
   UPDATE question
     SET reported = TRUE
     WHERE question_id = $1::INT
 `;
-router.put('/qa/questions/:question_id/report', asyncTry(async (req, res) => {
-  await sql.query({
-    name: 'report-question',
-    text: stmtReportQuestion,
-    values: [req.paramInt('question_id')],
-  });
-  res.sendStatus(204);
+router.put('/qa/questions/:question_id/report', (req, res) => tryPut(res, {
+  name: 'report-question',
+  text: stmtReportQuestion,
+  values: [req.paramInt('question_id')],
 }));
 
 const stmtReportAnswer = `
@@ -23,13 +29,10 @@ const stmtReportAnswer = `
     SET reported = TRUE
     WHERE id = $1::INT
 `;
-router.put('/qa/answers/:answer_id/report', asyncTry(async (req, res) => {
-  await sql.query({
-    name: 'report-answer',
-    text: stmtReportAnswer,
-    values: [req.paramInt('answer_id')],
-  });
-  res.sendStatus(204);
+router.put('/qa/answers/:answer_id/report', (req, res) => tryPut(res, {
+  name: 'report-answer',
+  text: stmtReportAnswer,
+  values: [req.paramInt('answer_id')],
 }));
 
 const stmtHelpfulQuestion = `
@@ -37,13 +40,10 @@ const stmtHelpfulQuestion = `
     SET question_helpfulness = question_helpfulness + 1
     WHERE question_id = $1::INT
 `;
-router.put('/qa/questions/:question_id/helpful', asyncTry(async (req, res) => {
-  await sql.query({
-    name: 'helpful-question',
-    text: stmtHelpfulQuestion,
-    values: [req.paramInt('question_id')],
-  });
-  res.sendStatus(204);
+router.put('/qa/questions/:question_id/helpful', (req, res) => tryPut(res, {
+  name: 'helpful-question',
+  text: stmtHelpfulQuestion,
+  values: [req.paramInt('question_id')],
 }));
 
 const stmtHelpfulAnswer = `
@@ -51,13 +51,10 @@ const stmtHelpfulAnswer = `
     SET helpfulness = helpfulness + 1
     WHERE id = $1::INT
 `;
-router.put('/qa/answers/:answer_id/helpful', asyncTry(async (req, res) => {
-  await sql.query({
-    name: 'helpful-answer',
-    text: stmtHelpfulAnswer,
-    values: [req.paramInt('answer_id')],
-  });
-  res.sendStatus(204);
+router.put('/qa/answers/:answer_id/helpful', (req, res) => tryPut(res, {
+  name: 'helpful-answer',
+  text: stmtHelpfulAnswer,
+  values: [req.paramInt('answer_id')],
 }));
 
 module.exports = router;
--- routes/put.spec.js
+++ routes/put.spec.js
@@ -31,7 +31,10 @@ const getAnswers = async () =>
 
 describe('PUT /qa/questions/:question_id/helpful', () => {
   it('increments helpfulness by 1', async () => {
-    await request(app).put('/qa/questions/2/helpful');
+    await request(app)
+      .put('/qa/questions/2/helpful')
+      .expect(204);
+
     const { rows } = await getQuestions();
     const helpfulness = rows
       .map(question => question.question_helpfulness);
@@ -42,6 +45,7 @@ describe('PUT /qa/questions/:question_id/helpful', () => {
     await Promise.all(Array(100).fill(1).map(async () =>
       request(app).put('/qa/questions/1/helpful')
     ));
+
     const { rows } = await getQuestions();
     expect(rows[0].question_helpfulness).toEqual(100);
   });
@@ -49,7 +53,10 @@ describe('PUT /qa/questions/:question_id/helpful', () => {
 
 describe('PUT /qa/answers/:answer_id/helpful', () => {
   it('increments helpfulness by 1', async () => {
-    await request(app).put('/qa/answers/2/helpful');
+    await request(app)
+      .put('/qa/answers/2/helpful')
+      .expect(204);
+
     const { rows } = await getAnswers();
     const helpfulness = rows
       .map(answer => answer.helpfulness);
@@ -60,6 +67,7 @@ describe('PUT /qa/answers/:answer_id/helpful', () => {
     await Promise.all(Array(100).fill(1).map(async () =>
       request(app).put('/qa/answers/1/helpful')
     ));
+
     const { rows } = await getAnswers();
     expect(rows[0].helpfulness).toEqual(100);
   });
@@ -67,7 +75,10 @@ describe('PUT /qa/answers/:answer_id/helpful', () => {
 
 describe('PUT /qa/questions/:question_id/report', () => {
   it('reports a question', async () => {
-    await request(app).put('/qa/questions/2/report');
+    await request(app)
+      .put('/qa/questions/2/report')
+      .expect(204);
+
     const { rows } = await getQuestions();
     const reported = rows
       .map(question => question.reported);
@@ -78,6 +89,7 @@ describe('PUT /qa/questions/:question_id/report', () => {
     await Promise.all(Array(100).fill(1).map(async () =>
       request(app).put('/qa/questions/1/report')
     ));
+
     const { rows } = await getQuestions();
     expect(rows[0].reported).toEqual(true);
   });
@@ -85,7 +97,10 @@ describe('PUT /qa/questions/:question_id/report', () => {
 
 describe('PUT /qa/answers/:answer_id/report', () => {
   it('reports a question', async () => {
-    await request(app).put('/qa/answers/2/report');
+    await request(app)
+      .put('/qa/answers/2/report')
+      .expect(204);
+
     const { rows } = await getAnswers();
     const reported = rows
       .map(answer => answer.reported);
@@ -96,6 +111,7 @@ describe('PUT /qa/answers/:answer_id/report', () => {
     await Promise.all(Array(100).fill(1).map(async () =>
       request(app).put('/qa/answers/1/report')
     ));
+
     const { rows } = await getAnswers();
     expect(rows[0].reported).toEqual(true);
   });
